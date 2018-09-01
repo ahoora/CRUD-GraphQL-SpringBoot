4b640aeda9f3cd0fc3a2d50993ed15fa860462dd
joegabonito <joejung5678@gmail.com>
2018-08-31 23:37:29 -0400

    JBOSS Configuration finished

--- src/main/java/com/joegabonito/graphqlspringboot/GraphqlSpringbootApplication.java
+++ src/main/java/com/joegabonito/graphqlspringboot/GraphqlSpringbootApplication.java
@@ -2,10 +2,15 @@ package com.joegabonito.graphqlspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GraphqlSpringbootApplication {
-
+public class GraphqlSpringbootApplication extends SpringBootServletInitializer {
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GraphqlSpringbootApplication.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(GraphqlSpringbootApplication.class, args);
 	}
